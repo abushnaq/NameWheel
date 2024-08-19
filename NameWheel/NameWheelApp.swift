ed2e44fd839b23ac3fd8639eaddafbae91e5786a
Ahmad Bushnaq <abushnaq@gmail.com>
2024-08-19 11:41:02 -0400

    Initial Commit

---
+++ NameWheel/NameWheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameWheelApp.swift
+//  NameWheel
+//
+//  Created by Ahmad Remote on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct NameWheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
